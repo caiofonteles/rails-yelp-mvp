f7cdb57e379420e299354e48661afff820485e1f
caio fonteles <fontelescaio@gmail.com>
2020-04-22 00:08:32 +0000

    finish controllers

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,31 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
+
+  def index
+    @restaurants = Restaurants.all
+  end
+
+  def show; end
+
+  def new
+    @restaurant = Restaurants.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurants.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating, :category)
+  end
 end
